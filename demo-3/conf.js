c933d6ef96f2a64f986f1313e27a253b3c741f50
bshawn <bret.shawn@gmail.com>
2019-11-07 17:04:29 -0500

    Switch demo 3 to chrome

--- demo-3/conf.js
+++ demo-3/conf.js
@@ -2,6 +2,6 @@ exports.config = {
   seleniumAddress: "http://localhost:4444/wd/hub",
   specs: ["conduit-spec.js"],
   capabilities: {
-    browserName: "firefox"
+    browserName: "chrome"
   }
 };
